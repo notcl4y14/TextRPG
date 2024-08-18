8cd9bbd1dabc7ee185653bb6064ab790f26a20e1
notcl4y14 <notcl4y@gmail.com>
2024-08-18 12:32:28 +0300

    Add debug_list_entities and debug_list_commands

---
+++ Content/Commands/DebugList.cs
@@ -0,0 +1,55 @@
+using Common;
+using Core;
+
+namespace Content.Commands;
+
+class DebugListItems : Command
+{
+	public DebugListItems()
+	{
+		Name = "debug_list_items";
+		Alias = [];
+	}
+
+	public override void Run(string[] args, ref Entity entity)
+	{
+		foreach (var item in ItemLibrary.Registers)
+		{
+			Console.WriteLine($"{item.Key}: {item.Value}");
+		}
+	}
+}
+
+class DebugListEntities : Command
+{
+	public DebugListEntities()
+	{
+		Name = "debug_list_entities";
+		Alias = [];
+	}
+
+	public override void Run(string[] args, ref Entity entity)
+	{
+		foreach (var ent in EntityLibrary.Registers)
+		{
+			Console.WriteLine($"{ent.Key}: {ent.Value}");
+		}
+	}
+}
+
+class DebugListCommands : Command
+{
+	public DebugListCommands()
+	{
+		Name = "debug_list_commands";
+		Alias = [];
+	}
+
+	public override void Run(string[] args, ref Entity entity)
+	{
+		foreach (var command in CommandLibrary.Registers)
+		{
+			Console.WriteLine($"{command.Key}: {command.Value}");
+		}
+	}
+}
